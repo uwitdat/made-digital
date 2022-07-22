2df3ae612adabf3e8f1aa282c99941d38f89d7c6
uwitdat <ben.shekhtman@myplanet.com>
2022-07-22 17:21:28 -0400

    styling updates

--- app/components/services-tab/Services.jsx
+++ app/components/services-tab/Services.jsx
@@ -1,69 +1,114 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import styles from './Services.css';
 import { services, titles } from './data';
 import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
+import useMediaQuery from '../../hooks/useMediaQuery';
 
 export function links() {
-  return [
-    { rel: "stylesheet", href: styles }
-  ];
+  return [{ rel: 'stylesheet', href: styles }];
 }
 
 const Services = () => {
-
   const [current, setCurrent] = useState(0);
-  const [currentTitle, setCurrentTitle] = useState(titles[0].ref)
+  const [currentTitle, setCurrentTitle] = useState(titles[0].ref);
   const [fade, setFade] = useState(false);
 
   const handleChangePanel = (ref) => {
     if (current === ref) return;
     setCurrentTitle(ref);
     setFade(true);
-    setTimeout(() => { setCurrent(ref) }, 290);
-    setTimeout(() => { setFade(false) }, 292);
-  }
+    setTimeout(() => {
+      setCurrent(ref);
+    }, 290);
+    setTimeout(() => {
+      setFade(false);
+    }, 292);
+  };
+
+  const isMobile = useMediaQuery('(max-width: 1300px)');
 
   const options = {
     root: null,
     rootMargin: '10px',
-    threshold: 0.5
-  }
+    threshold: isMobile ? 0.1 : 0.5,
+  };
 
   const [container, isVisible] = useIntersectionObserver(options);
 
+  const [active, setActive] = useState(0);
+
+  const handleSetActive = (idx) => {
+    if (active === idx) {
+      return;
+    } else {
+      setActive(idx);
+    }
+  };
+
   return (
-    <div ref={container} className={isVisible ? 'Services clear fade-in' : 'Services clear'} id='services'>
-
+    <div
+      ref={container}
+      className={isVisible ? 'Services clear fade-in' : 'Services clear'}
+      id="services"
+    >
       <h1>How We Help</h1>
-      <div className='services-container'>
-        <div className='services-nav-col'>
-          <ul className='services-nav'>
-            {titles.map(({ title, ref, icon }, idx) => (
-              <li onClick={() => handleChangePanel(ref)} id={currentTitle === ref ? 'service-active' : ''} key={idx}>{title}</li>
-            ))}
-          </ul>
-        </div>
+      <div className="services-container">
+        {!isMobile ? (
+          <div className="services-nav-col">
+            <ul className="services-nav">
+              {titles.map(({ title, ref, icon }, idx) => (
+                <li
+                  onClick={() => handleChangePanel(ref)}
+                  id={currentTitle === ref ? 'service-active' : ''}
+                  key={idx}
+                >
+                  {title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
 
-        <div className='services-content-col'>
-
-          <section className='service'>
-            <div className={fade ? 'service-container out' : 'service-container in'}>
-              <div className='service-titles'>
-                <h2>{services[current].title}</h2>
-                <span>{services[current].icon}</span>
-              </div>
-              <p>{services[current].des}</p>
+        <div className="services-content-col">
+          <section className="service">
+            <div
+              className={
+                fade ? 'service-container out' : 'service-container in'
+              }
+            >
+              {!isMobile ? (
+                <>
+                  <div className="service-titles">
+                    <h2>{services[current].title}</h2>
+                    <span>{services[current].icon}</span>
+                  </div>
+                  <p>{services[current].des}</p>
+                </>
+              ) : (
+                services.map((service, idx) => (
+                  <div
+                    onClick={() => handleSetActive(idx)}
+                    className={
+                      idx == active
+                        ? 'service-one service-active'
+                        : 'service-one'
+                    }
+                    key={idx}
+                  >
+                    <div className="service-titles">
+                      <h2>{service.title}</h2>
+                      <span>{service.icon}</span>
+                    </div>
+                    <p>{service.des}</p>
+                  </div>
+                ))
+              )}
             </div>
           </section>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-
-
-export default Services
-
+export default Services;
